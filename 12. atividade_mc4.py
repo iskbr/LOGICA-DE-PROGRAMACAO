0737fb2e1e4554a96644104b2ca6ce6bda7f3b22
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-03 17:02:32 -0300

    atividade imcompleta

---
+++ 12. atividade_mc4.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+print("""
+\t Opções para os sexos:
+M - Masculino
+F - Feminino
+
+""")
+sexo = str(input("Selecione seu sexo: "))
+altura = float(input("Digite sua altura: "))
+match sexo:
+    case "M":
+    print("Peso ideal.")
+    case
