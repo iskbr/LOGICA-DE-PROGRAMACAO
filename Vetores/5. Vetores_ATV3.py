3be35257d85f6922a1fecb334f224d4cc3ac3608
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-01 15:29:20 -0300

    feat: atividade sobre os 5 números

---
+++ Vetores/5. Vetores_ATV3.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+lista_numeros = []
+
+for i in range(5):
+    numeros = float(input(f"Digite o {i+1}° número: "))
+    lista_numeros.append(numeros)
+
+for i in range(5):
+    print(f"Números: {lista_numeros[i]}")
+
+print(f"Maior número: {max(lista_numeros)}")
+print(f"Menor número: {min(lista_numeros)}")
